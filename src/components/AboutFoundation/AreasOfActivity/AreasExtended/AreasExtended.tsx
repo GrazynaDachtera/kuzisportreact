1cb744cb9c4da484555bd740a7b6b2a869157f23
GrazynaDachtera <grazynadachtera@gmail.com>
2025-08-27 19:28:41 +0200

    added all areas of activity description

--- src/components/AboutFoundation/AreasOfActivity/AreasExtended/AreasExtended.tsx
+++ src/components/AboutFoundation/AreasOfActivity/AreasExtended/AreasExtended.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 import "./AreasExtended.scss";
 
@@ -19,39 +18,158 @@ type ActivityItem = {
 };
 
 type AreasOfActivityProps = {
-  title?: string;
   items?: ActivityItem[];
   image?: Img;
 };
 
 const DEFAULT_ITEMS: ActivityItem[] = [
   {
-    title: "Robimy Porzundek!",
-    text: "chodzimy po Łazarzu wzdłuż i wszerz – nieporządek zgłaszamy komu trzeba i pilnujemy realizacji zadań porządkowych przez służby miejskie",
+    title:
+      "Działanie na rzecz integracji, wzmacniania więzi międzyludzkich, znoszenia barier międzypokoleniowych i kulturowych wśród lokalnych społeczności oraz wspólnot: seniorów, osób z niepełnosprawnościami, rodzin wielodzietnych, dzieci i młodzieży, osób samotnych oraz samodzielnie wychowujących dzieci, osób zagrożonych wykluczeniem;",
   },
   {
-    title: "Integrujemy Łazarz i sąsiedztwo!",
-    text: "organizujemy wydarzenia sąsiedzkie, od spacerów po pikniki i warsztaty dla mieszkańców, dbając o walor edukacyjny",
+    title:
+      "Działalność wspomagająca rozwój wspólnot i społeczności lokalnych, integracji i reintegracji zawodowej i społecznej osób zagrożonych wykluczeniem społecznym;",
   },
   {
-    title: "Zieleń i zwierzęta, natura!",
-    text: "naszym priorytetem jest jeszcze więcej dobrze zaplanowanej zieleni na Łazarzu, miejsc przyjemnych dla ludzi i innych stworków – zawsze to uwzględniamy w naszych opiniach i projektach",
+    title:
+      "Krzewienie i propagowanie wzorców współżycia społecznego i postaw obywatelskich;",
   },
   {
-    title: "Inwestycje!",
-    text: "wiemy jak dobrze planować, wydawać pieniądze i pilnować trwałości inwestycji. Staramy się o kompromis dla potrzeb pieszych, rowerzystów, kierowców, młodszych, starszych, chytrych i estetów",
+    title:
+      "Działanie na rzecz popularyzacji: wiedzy historycznej, świadomości obywatelskiej, tradycji lokalnych, miejskich oraz narodowych, w tym działania na rzecz upamiętniania osób, miejsc, wydarzeń, renowacji obiektów, rewitalizacji;",
   },
   {
-    title: "Kultura i historia!",
-    text: "co, gdzie, kiedy – uważnie śledzimy i nagłaśniamy informacje o wydarzeniach na Łazarzu, a także w całym Poznaniu: takich które właśnie się dzieją i takich które już się działy. Organizujemy spacery z historią Łazarza w tle",
+    title:
+      "Działalność na rzecz zwiększania świadomości prawnej społeczeństwa;",
   },
   {
-    title: "… i wiele więcej!",
+    title:
+      "Działalność na rzecz podtrzymywania i upowszechniania tradycji narodowej, pielęgnowania polskości oraz rozwoju świadomości narodowej, obywatelskiej i kulturowej, a także dla mniejszości narodowych i etnicznych oraz języka regionalnego, a tym na rzecz integracji cudzoziemców;",
+  },
+  { title: "Działanie na rzecz rozwoju turystyki lokalnej i krajoznawstwa;" },
+  {
+    title:
+      "Działanie na rzecz promocji zdrowego trybu życia, aktywności fizycznej i uprawiania sportu; promocja ochrony zdrowia, wspieranie i upowszechnianie kultury fizycznej;",
+  },
+  {
+    title:
+      "Działanie na rzecz edukacji artystycznej i kulturalnej, rozwoju umiejętności oraz kształcenia ustawicznego osób w każdym wieku;",
+  },
+  {
+    title:
+      "Działanie na rzecz zwiększenia dostępności do kultury, edukacji, działań artystycznych oraz rozwoju kultury i sztuki, wspieranie i promocja lokalnych twórców i artystów;",
+  },
+  {
+    title:
+      "Inicjowanie i wspieranie przekształcania oraz tworzenia przestrzeni dostępnych dla osób ze szczególnymi potrzebami; działalność na rzecz osób niepełnosprawnych, a także na rzecz osób w wieku emerytalnym;",
+  },
+  {
+    title:
+      "Wspieranie lokalnej przedsiębiorczości i innowacyjnych inicjatyw, ich rozwoju;",
+  },
+  { title: "Działalność na rzecz równych praw kobiet i mężczyzn;" },
+  {
+    title:
+      "Działalność wspomagająca rozwój techniki, wynalazczości i innowacyjności oraz rozpowszechnianie i wdrażanie nowych rozwiązań technicznych w praktyce gospodarczej;",
+  },
+  {
+    title:
+      "Działalność na rzecz dzieci i młodzieży, upowszechniania i ochrony praw dziecka;",
+  },
+  {
+    title:
+      "Promowanie ekologii i ochrony zwierząt oraz ochrony dziedzictwa przyrodniczego;",
+  },
+  {
+    title:
+      "Działalność na rzecz porządku i bezpieczeństwa publicznego, ratownictwa i ochrony ludności, obronności państwa i działalności Sił Zbrojnych Rzeczypospolitej Polskiej;",
+  },
+  {
+    title:
+      "Upowszechnianie i ochrona wolności i praw człowieka oraz swobód obywatelskich, a także działania wspomagające rozwój demokracji;",
+  },
+  {
+    title:
+      "Działalność na rzecz nieodpłatnego poradnictwa obywatelskiego, upowszechniania i ochrony praw konsumentów;",
+  },
+  {
+    title:
+      "Działalność na rzecz integracji europejskiej oraz rozwijania kontaktów i współpracy między społeczeństwami;",
+  },
+  {
+    title:
+      "Działalność na rzecz kombatantów i osób represjonowanych, weteranów i weteranów poszkodowanych w rozumieniu ustawy z dnia 19 sierpnia 2011 r. o weteranach działań poza granicami państwa (Dz. U. z 2023 r. poz. 2112);",
+  },
+  { title: "Działalność na rzecz rodziny, macierzyństwa, rodzicielstwa;" },
+  {
+    title:
+      "Działalność na rzecz przeciwdziałania uzależnieniom i patologiom społecznym;",
+  },
+  {
+    title:
+      "Prowadzenie działalności informacyjnej, stron, portali internetowych i społecznościowych, kampanii społecznych;",
+  },
+  {
+    title:
+      "Współdziałanie z władzami samorządowymi, rządowymi, mediami, placówkami oświatowymi, innymi organizacjami pozarządowymi oraz lokalnymi wspólnotami i społecznościami;",
+  },
+  {
+    title:
+      "Wydawanie ulotek, czasopism, broszur, książek, albumów i artykułów związanych z celami Stowarzyszenia;",
+  },
+  {
+    title:
+      "Tworzenie różnego rodzaju projektów związanych z integracją, kulturą, sztuką, historią i turystyką, prowadzenie działalności kulturalnej;",
+  },
+  {
+    title:
+      "Organizowanie wydarzeń rozrywkowo-kulturalnych, spotkań towarzyskich i integracyjnych, pikników, koncertów, projekcji filmowych i festiwali, spotkań autorskich, konferencji, szkoleń, pokazów i wykładów;",
+  },
+  {
+    title:
+      "Organizowanie zawodów i konkursów związanych z celami Stowarzyszenia;",
+  },
+  {
+    title:
+      "Organizowanie, prowadzenie i branie udziału w wydarzeniach artystycznych, warsztatach twórczych, zajęciach, spektaklach, wystawach i konkursach związanych z kulturą, sztuką, muzyką, fotografią, rękodziełem, rzemiosłem, plastyką, turystyką, filmografią, architekturą, krajoznawstwem i historią;",
+  },
+  {
+    title:
+      "Organizowanie i branie udziału w spotkaniach, rajdach, wycieczkach i wyjazdach integracyjnych, krajoznawczych i rekreacyjnych, a także tworzenie, organizowanie i prowadzenie gier terenowych i spacerów;",
+  },
+  {
+    title:
+      "Organizowanie czasu wolnego dla dzieci, młodzieży, osób dorosłych i seniorów;",
+  },
+  {
+    title:
+      "Udzielanie wsparcia pozafinansowego osobom w trudnej sytuacji życiowej;",
+  },
+  {
+    title:
+      "Realizowanie spektakli, filmów, utworów, nagrań video i innych form sztuki związanych z celami Stowarzyszenia;",
+  },
+  {
+    title:
+      "Działanie na rzecz powstawania i renowacji obiektów upamiętniających osoby, miejsca i wydarzenia związane z historią, krajoznawstwem, kulturą i sztuką;",
+  },
+  {
+    title:
+      "Gromadzenie środków na działalność Stowarzyszenia z funduszy zewnętrznych oraz własnych i zbiórek publicznych;",
+  },
+  {
+    title:
+      "Zatrudnianie pracowników, w tym członków Stowarzyszenia, zlecanie zadań podmiotom zewnętrznym w związku z realizowaniem celów Stowarzyszenia;",
+  },
+  { title: "Prowadzenie nieodpłatnej działalności pożytku publicznego;" },
+  {
+    title:
+      "Działanie na rzecz podnoszenia kwalifikacji i kompetencji członków Stowarzyszenia.",
   },
 ];
 
 export default function AreasOfActivity({
-  title = "Obszary działalności",
   items = DEFAULT_ITEMS,
   image = {
     src: "/News/matejki-poznan.png",
@@ -66,32 +184,22 @@ export default function AreasOfActivity({
       <div className="areasextended-container">
         <div className="areasextended-top">
           <div className="areasextended-content">
-            <h2 className="areasextended-title">{title}</h2>
-
             <div className="areasextended-description">
               <ul className="areasextended-list" role="list">
                 {items.map((item, idx) => (
                   <li key={idx} className="areasextended-item">
-                    <h3 className="areasextended-item-title">{item.title}</h3>
+                    <h3 className="areasextended-item-title">
+                      <span className="areasextended-item-number">
+                        {idx + 1}.
+                      </span>
+                      {item.title}
+                    </h3>
                     {item.text ? (
                       <div className="areasextended-item-text">{item.text}</div>
                     ) : null}
                   </li>
                 ))}
               </ul>
-
-              <Link href="/AreasOfActivity" className="areasextended-cta">
-                Poznaj wszystkie obszary naszej działalności
-                <svg
-                  className="areasextended-cta-icon"
-                  viewBox="0 0 24 24"
-                  aria-hidden="true"
-                  focusable="false"
-                >
-                  <path d="M6 6 L18 18" />
-                  <path d="M12 18 H18 V12" />
-                </svg>
-              </Link>
             </div>
           </div>
 
