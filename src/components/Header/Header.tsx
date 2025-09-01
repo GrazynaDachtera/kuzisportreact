b2b84c6f9f22773c73bf202559771251a61f044d
GrazynaDachtera <grazynadachtera@gmail.com>
2025-09-01 16:29:38 +0200

    added changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,7 +12,7 @@ interface Slide {
 
 const slides: Slide[] = [
   {
-    titleLines: ["Sąsiedzki", "Łazarz"],
+    titleLines: ["Kuzi", "Sport"],
     cta: { label: "Sprawdź", href: "/AboutFoundation" },
     img: { src: "/Header/2.jpg", alt: "z", width: 1600, height: 1067 },
   },
